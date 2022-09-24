cc26afeb71928b778f6bcc787755b54988ef6559
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2022-09-24 17:20:30 +0300

    Add "where" operator to index the file

--- src/lib/operators/index.ts
+++ src/lib/operators/index.ts
@@ -1,6 +1,7 @@
 export { run } from './run';
 export { log } from './log';
 export { push } from './push';
+export { where } from './where';
 export { numerate, numerateArrays, numerateObjects } from './numerate';
 export { join, joinArrays, joinObjects } from './join';
 export { addFieldToArray, addFieldToObject, addField } from './addField';
