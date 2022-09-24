1660195adccd6cf88c9d984bd37d6b1f79def5fd
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2022-09-24 19:03:01 +0300

    Add overloaded versions of where() operator

--- src/lib/operators/where.ts
+++ src/lib/operators/where.ts
@@ -1,6 +1,18 @@
 import { filter } from "rxjs";
 
 // Synonym for operator "filter" 
-export function where<T>(predicate: (value: T, index: number) => boolean) {
-    return filter<T>(predicate);
+export function where<T>(criteria: {});
+export function where<T>(predicate: (value: T, index: number) => boolean);
+export function where<T>(predicate: {} | ((value: T, index: number) => boolean)) {
+    if (typeof predicate === 'function') return filter<T>(predicate as (value: T, index: number) => boolean);
+    else {
+        return filter<T>((value: T) => {
+            for (const key in predicate) {
+                if (predicate.hasOwnProperty(key)) {
+                    if (predicate[key] != value[key]) return false;
+                }
+            }
+            return true;
+        });
+    }
 }
