e7dc1c562414b21f17cfb1b8f3b9b2d75143d49c
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2022-09-24 19:02:30 +0300

    Change run() implementation to more simple

--- src/lib/operators/run.ts
+++ src/lib/operators/run.ts
@@ -1,7 +1,13 @@
 import { lastValueFrom, Observable, forkJoin, first } from "rxjs";
 
 export async function run(...observables: Observable<any>[]) {
-    const observable = forkJoin([...observables]);
-    await lastValueFrom<any>(observable);
+    //const observable = forkJoin([...observables]);
+    //await lastValueFrom<any>(observable);
+
     //await observable.toPromise();
+
+    const promises: Promise<any>[] = [];
+    for (const observable of observables) promises.push(lastValueFrom<any>(observable));
+    await Promise.all(promises);
+    
 }
