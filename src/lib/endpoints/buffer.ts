6717abf6418868fb42efe5ff8eb9f6c2f589d91d
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2022-09-26 10:04:10 +0300

    Add "buffer" property to access internal buffer

--- src/lib/endpoints/buffer.ts
+++ src/lib/endpoints/buffer.ts
@@ -3,18 +3,22 @@ import { Endpoint, EndpointImpl } from "../core/endpoint";
 import { EtlObservable } from "../core/observable";
 
 export class BufferEndpoint<T = any> extends EndpointImpl<T> {
-    protected buffer: T[];
+    protected _buffer: T[];
+
+    get buffer() {
+        return this._buffer;
+    }
 
     constructor(...values: T[]) {
         super();
-        this.buffer = [...values];
+        this._buffer = [...values];
     }
 
     public read(): EtlObservable<T> {
         const observable = new EtlObservable<T>((subscriber) => {
             try {
                 this.sendStartEvent();
-                this.buffer.forEach(value => {
+                this._buffer.forEach(value => {
                     this.sendDataEvent(value);
                     subscriber.next(value);
                 });
@@ -31,21 +35,21 @@ export class BufferEndpoint<T = any> extends EndpointImpl<T> {
 
     public async push(value: T) {
         super.push(value);
-        this.buffer.push(value); 
+        this._buffer.push(value); 
     }
 
     public async clear() {
         super.clear();
-        this.buffer = [];
+        this._buffer = [];
     }
 
     public sort(compareFn: ((v1: T, v2: T) => number | boolean) | undefined = undefined): void {
         if (compareFn === undefined) {
-            this.buffer.sort();
+            this._buffer.sort();
             return;
         }
 
-        this.buffer.sort((v1: T, v2: T) => {
+        this._buffer.sort((v1: T, v2: T) => {
             let res = compareFn(v1, v2);
             if (typeof res === "boolean") res = res? 1 : -1;
             return res;
@@ -53,6 +57,6 @@ export class BufferEndpoint<T = any> extends EndpointImpl<T> {
     }
 
     public forEach(callbackfn: (value: T, index: number, array: T[]) => void) {
-        this.buffer.forEach(callbackfn);
+        this._buffer.forEach(callbackfn);
     }
 }
