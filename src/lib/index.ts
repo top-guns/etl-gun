7c5a33547376c6e6a3a6a7f77dab19c43537a8cb
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-04-17 18:53:03 +0300

    minor changes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,29 @@ export * from './core/gui.js';
 export * from './operators/index.js';
 export * from './helpers/index.js';
 export * from './utils/index.js';
+
+import * as trello from './endpoints/trello/index.js'
+import * as csv from './endpoints/csv.js'
+import * as filesystem from './endpoints/filesystem.js'
+import * as interval from './endpoints/interval.js'
+import * as json from './endpoints/json.js'
+import * as magento from './endpoints/magento.js'
+import * as memory from './endpoints/memory.js'
+import * as mysql from './endpoints/mysql.js'
+import * as postgres from './endpoints/postgres.js'
+import * as telegram from './endpoints/telegram.js'
+import * as xml from './endpoints/xml.js'
+
+export {
+    trello,
+    csv,
+    filesystem,
+    interval,
+    json,
+    magento,
+    memory,
+    mysql,
+    postgres,
+    telegram,
+    xml
+}
