911cc1101774d44b5bf99405331dcb986be16fce
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-04-26 15:41:56 +0300

    Add some 'push' variants

--- src/lib/operators/push.ts
+++ src/lib/operators/push.ts
@@ -2,20 +2,27 @@ import { mergeMap, from } from "rxjs";
 import { BaseCollection } from "../core/collection.js";
 import { GuiManager } from "../index.js";
 
-export function push<T>(collection: BaseCollection<T>, ...params: any[]) {
+
+export function push<T>(collection: BaseCollection<T>, value: T);
+export function push<T>(collection: BaseCollection<T>, callback: (value: T) => Promise<any>);
+export function push<T>(collection: BaseCollection<T>, value: any) {
     //return tap<T>(v => collection.insert(v, params));
     const f = async (v: T) => {
-        collection.insert(v, params);
+        // No wait
+        let vv = await getValue(v, value);
+        collection.insert(vv);
         return v;
     }
     const observable = (v: T) => from(f(v));
     return mergeMap((v: T)=> observable(v)); 
 }
 
-export function pushAndLog<T>(collection: BaseCollection<T>, ...params: any[]) {
-    //return tap<T>(v => collection.insert(v, params));
+export function pushAndLog<T>(collection: BaseCollection<T>, value: T);
+export function pushAndLog<T>(collection: BaseCollection<T>, callback: (value: T) => Promise<any>);
+export function pushAndLog<T>(collection: BaseCollection<T>, value: any) {
     const f = async (v: T) => {
-        const res = await collection.insert(v, params);
+        let vv = await getValue(v, value);
+        const res = await collection.insert(vv);
         GuiManager.log(res);
         return v;
     }
@@ -23,22 +30,33 @@ export function pushAndLog<T>(collection: BaseCollection<T>, ...params: any[]) {
     return mergeMap((v: T)=> observable(v)); 
 }
 
-export function pushAndGet<T>(collection: BaseCollection<T>, ...params: any[]) {
-    //return tap<T>(v => collection.insert(v, params));
+
+export function pushAndGet<T>(collection: BaseCollection<T>, value: T);
+export function pushAndGet<T>(collection: BaseCollection<T>, callback: (value: T) => Promise<any>);
+export function pushAndGet<T>(collection: BaseCollection<T>, value: any) {
     const f = async (v: T) => {
-        const res = await collection.insert(v, params);
+        let vv = await getValue(v, value);
+        const res = await collection.insert(vv);
         return res;
     }
     const observable = (v: T) => from(f(v));
     return mergeMap((v: T)=> observable(v)); 
 }
 
-export function pushAndWait<T>(collection: BaseCollection<T>, ...params: any[]) {
-    //return tap<T>(v => collection.insert(v, params));
+export function pushAndWait<T>(collection: BaseCollection<T>, value: T);
+export function pushAndWait<T>(collection: BaseCollection<T>, callback: (value: T) => Promise<any>);
+export function pushAndWait<T>(collection: BaseCollection<T>, value: any) {
     const f = async (v: T) => {
-        const res = await collection.insert(v, params);
+        let vv = await getValue(v, value);
+        const res = await collection.insert(vv);
         return v;
     }
     const observable = (v: T) => from(f(v));
     return mergeMap((v: T)=> observable(v)); 
+}
+
+async function getValue<T = any>(streamValue: T, paramValue?: T | ((value: T) => Promise<any>) | any): Promise<any> {
+    if (typeof paramValue == 'undefined') return streamValue;
+    if (typeof paramValue == 'function') paramValue = await (paramValue as (value: T) => Promise<any>)(streamValue);
+    return paramValue;
 }
