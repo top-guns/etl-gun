e9cef50795db6848c13e145c08a0c48e9d69b546
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-05-01 14:56:06 +0300

    Add toProperty mechanism

--- src/lib/operators/push.ts
+++ src/lib/operators/push.ts
@@ -1,16 +1,20 @@
 import { mergeMap, from, OperatorFunction, Observable } from "rxjs";
+import _ from 'lodash';
 import { BaseCollection } from "../core/collection.js";
 import { GuiManager } from "../index.js";
 
 
-export function push<S>(collection: BaseCollection<S>): OperatorFunction<S, S>;
-export function push<S>(collection: BaseCollection<S>, value: S): OperatorFunction<S, S>;
-export function push<S, T = S>(collection: BaseCollection<T>, callback: (value: S) => (T | Promise<T>)): OperatorFunction<S, S>;
-export function push<S, T = S>(collection: BaseCollection<T>, paramValue?: T | ((value: S) => (T | Promise<T>))): OperatorFunction<S, S> {
+type PushOptions<S, T> = {
+    fromProperty: string;
+    value: S | ((value: S) => (T | Promise<T>));
+}
+
+
+export function push<S, T=S>(collection: BaseCollection<T>, options?: PushOptions<S, T>): OperatorFunction<S, S> {
     //return tap<T>(v => collection.insert(v, params));
     const f = async (v: S) => {
         // No wait
-        let vv: T = await getValue<S, T>(v, paramValue);
+        let vv: T = await getValue<S, T>(v, options);
         collection.insert(vv);
         return v;
     }
@@ -18,12 +22,9 @@ export function push<S, T = S>(collection: BaseCollection<T>, paramValue?: T | (
     return mergeMap((v: S)=> observable(v)); 
 }
 
-export function pushAndLog<S>(collection: BaseCollection<S>): OperatorFunction<S, S>;
-export function pushAndLog<S>(collection: BaseCollection<S>, value: S): OperatorFunction<S, S>;
-export function pushAndLog<S, T = S>(collection: BaseCollection<T>, callback: (value: S) => (T | Promise<T>)): OperatorFunction<S, S>;
-export function pushAndLog<S, T = S>(collection: BaseCollection<T>, paramValue?: T | ((value: S) => (T | Promise<T>))): OperatorFunction<S, S> {
+export function pushAndLog<S, T=S>(collection: BaseCollection<T>, options?: PushOptions<S, T>): OperatorFunction<S, S> {
     const f = async (v: S) => {
-        let vv: T = await getValue<S, T>(v, paramValue);
+        let vv: T = await getValue<S, T>(v, options);
         const res = await collection.insert(vv);
         GuiManager.log(res);
         return v;
@@ -33,27 +34,20 @@ export function pushAndLog<S, T = S>(collection: BaseCollection<T>, paramValue?:
 }
 
 
-export function pushAndGet<S>(collection: BaseCollection<S>): OperatorFunction<S, S>;
-export function pushAndGet<S>(collection: BaseCollection<S>, value: S, toProperty?: string): OperatorFunction<S, S>;
-export function pushAndGet<S, T = S, R = S>(collection: BaseCollection<T>, callback: (value: S) => (T | Promise<T>), toProperty?: string): OperatorFunction<S, R>;
-export function pushAndGet<S, T = S, R = S>(collection: BaseCollection<T>, paramValue?: T | ((value: S) => (T | Promise<T>)), toProperty?: string): OperatorFunction<S, R> {
+export function pushAndGet<S, T, R>(collection: BaseCollection<T>, options?: PushOptions<S, T> & {toProperty: string}): OperatorFunction<S, R> {
     const f = async (v: S): Promise<R> => {
-        let vv: T = await getValue<S, T>(v, paramValue);
+        let vv: T = await getValue<S, T>(v, options);
         const res: R = await collection.insert(vv);
-        return getOperatorResult(v, toProperty, res);
+        return getOperatorResult(v, options.toProperty, res);
     }
 
     const observable: (value: S) => Observable<R> = (v: S) => from(f(v));
-
     return mergeMap<S, Observable<R>>((v: S)=> observable(v)); 
 }
 
-export function pushAndWait<S>(collection: BaseCollection<S>): OperatorFunction<S, S>;
-export function pushAndWait<S>(collection: BaseCollection<S>, value: S): OperatorFunction<S, S>;
-export function pushAndWait<S, T = S>(collection: BaseCollection<T>, callback?: (value: S) => (T | Promise<T>)): OperatorFunction<S, S>;
-export function pushAndWait<S, T = S>(collection: BaseCollection<T>, paramValue?: T | ((value: S) => (T | Promise<T>))): OperatorFunction<S, S> {
+export function pushAndWait<S, T=S>(collection: BaseCollection<T>, options?: PushOptions<S, T>): OperatorFunction<S, S> {
     const f = async (v: S) => {
-        let vv: T = await getValue<S, T>(v, paramValue);
+        let vv: T = await getValue<S, T>(v, options);
         await collection.insert(vv);
         return v;
     }
@@ -62,15 +56,16 @@ export function pushAndWait<S, T = S>(collection: BaseCollection<T>, paramValue?
 }
 
 
-async function getValue<S, T = S>(streamValue: S, paramValue?: T | ((value: S) => (T | Promise<T>))): Promise<T> {
-    if (typeof paramValue == 'undefined') return streamValue as unknown as T;
-    if (typeof paramValue == 'function') return await (paramValue as ((value: S) => (T | Promise<T>)))(streamValue);
-    return paramValue;
+async function getValue<S, T>(streamValue: S, options?: PushOptions<S, T>): Promise<T> {
+    if (typeof options == 'undefined') return streamValue as unknown as T;
+    if (typeof options.fromProperty) return _.get(streamValue, options.fromProperty) as unknown as T;
+    if (typeof options.value === 'function') return await (options.value as ((value: S) => (T | Promise<T>)))(streamValue);
+    return options.value as unknown as T;
 }
 
 async function getOperatorResult<S, R>(val: S, toProperty: string, res: any): Promise<R> {
     if (typeof toProperty === 'undefined') return res;
     if (!toProperty) return val as unknown as R; 
-    val[toProperty] = res;
+    _.set(val, toProperty, res);
     return val as unknown as R;
 }
