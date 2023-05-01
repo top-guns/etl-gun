d13543b3770a622c85e70ce584bf168d516f1262
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-05-01 04:25:00 +0300

    Add move operation support

--- src/lib/operators/toProperty.ts
+++ src/lib/operators/toProperty.ts
@@ -1,10 +1,13 @@
 import { map, OperatorFunction } from "rxjs";
 
-export function toProperty<T, R = T>(toProperty: string, fromProperty?: string): OperatorFunction<T, R> {
+export function toProperty<R, T = any>(toProperty: string, fromProperty?: string, operation: 'move' | 'copy' = 'copy'): OperatorFunction<T, R> {
     return map<T, R>(value => {
         let val = value;
         let res: any = value;
-        if (fromProperty) val = value[fromProperty];
+        if (fromProperty) {
+            val = value[fromProperty];
+            if (operation == 'move') delete value[fromProperty];
+        }
         else res = {};
         res[toProperty] = val;
         return res;
