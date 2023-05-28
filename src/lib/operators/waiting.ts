309355ccd5c114906052378161fcf741d33cbfd8
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-05-29 00:51:27 +0300

    Add functional parameter to waiting operator

--- src/lib/operators/waiting.ts
+++ src/lib/operators/waiting.ts
@@ -1,5 +1,9 @@
 import * as rx from "rxjs";
 
-export function waiting<T>(ms: number = 0): rx.OperatorFunction<T, T> {
-    return rx.concatMap(val => rx.of(val).pipe(rx.delay(ms)))
+export function waiting<T>(ms?: number): rx.OperatorFunction<T, T>;
+export function waiting<T>(getDelay: (v: T) => number): rx.OperatorFunction<T, T>;
+export function waiting<T>(delay?: number | ((v: T) => number)): rx.OperatorFunction<T, T> {
+    return rx.concatMap(val => rx.of(val).pipe(
+        rx.delay( !delay ? 0 : (typeof delay === 'number' ? delay : delay(val)) )
+    ))
 }
