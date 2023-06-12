676a44b7ce23dfae0ec602330916cf6d5c6b4fac
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-06-12 19:38:49 +0300

    Add more tests for the ftp endpoint

--- src/tests/suites/docker/endpoints/filesystems/ftp.test.ts
+++ src/tests/suites/docker/endpoints/filesystems/ftp.test.ts
@@ -1,10 +1,15 @@
-import { describe, test, before, after } from 'node:test';
+import { describe, test, before, after, beforeEach, afterEach } from 'node:test';
 import assert from 'node:assert';
 import * as rx from 'rxjs';
 import * as etl from '../../../../../lib/index.js';
+import { strictNullish, strictUndefined } from '../../../../utils.js';
+
+const ROOT_FOLDER = 'ftp';
 
 describe('FTP endpoint', () => {
     let ftp: etl.filesystems.Ftp.Endpoint;
+    let folder: etl.filesystems.Ftp.Collection;
+
     before(() => {
         ftp = new etl.filesystems.Ftp.Endpoint({host: "localhost", user: process.env.DOCKER_FTP_USER, password: process.env.DOCKER_FTP_PASSWORD});
     })
@@ -13,37 +18,54 @@ describe('FTP endpoint', () => {
         if (ftp) await ftp.releaseEndpoint();
     })
 
+    beforeEach(() => {
+        folder = ftp.getFolder(ROOT_FOLDER);
+    })
+
+    afterEach(() => {
+        ftp.releaseFolder(ROOT_FOLDER);
+        folder = null;
+    })
+
+    test('selectOne() missing path', async () => {
+        const value = await folder.selectOne('-------------------');
+        strictUndefined(value);
+    });
+
+    test('selectOne() existing empty folder', async () => {
+        const value = await folder.selectOne('dir-2');
+        assert.deepStrictEqual(value.path, 'dir-2');
+    });
+
+    test('selectOne() existing not empty folder', async () => {
+        const value = await folder.selectOne('dir-1/child-dir-1');
+        assert.deepStrictEqual(value.path, 'dir-1/child-dir-1');
+    });
+
+    test('selectOne() existing file', async () => {
+        const value = await folder.selectOne('dir-1/file-1.txt');
+        assert.deepStrictEqual(value.path, 'dir-1/file-1.txt');
+    });
+
     test('select()', async () => {
-        const ROOT_FOLDER = 'ftp/dir-1';
-
-        const folder = ftp.getFolder(ROOT_FOLDER);
-
-        const values = await folder.select();
+        const values = await folder.select('dir-1');
         const res: any[] = values.map(v => v.name);
 
         assert.deepStrictEqual(res, ['child-dir-1', 'file-1.txt', 'file-2.txt']);
     });
 
     test('selectGen()', async () => {
-        const ROOT_FOLDER = 'ftp/dir-1';
-
-        const folder = ftp.getFolder(ROOT_FOLDER);
-
         const res: any[] = [];
 
-        for await (const v of folder.selectGen()) res.push(v.name);
+        for await (const v of folder.selectGen('dir-1')) res.push(v.name);
 
         assert.deepStrictEqual(res, ['child-dir-1', 'file-1.txt', 'file-2.txt']);
     });
 
     test('selectIx()', async () => {
-        const ROOT_FOLDER = 'ftp/dir-1';
-
-        const folder = ftp.getFolder(ROOT_FOLDER);
-
         const res: any[] = [];
 
-        await folder.selectIx().forEach(v => {
+        await folder.selectIx('dir-1').forEach(v => {
             res.push(v.name);
         })
 
@@ -51,13 +73,9 @@ describe('FTP endpoint', () => {
     });
 
     test('selectRx()', async () => {
-        const ROOT_FOLDER = 'ftp/dir-1';
-
-        const folder = ftp.getFolder(ROOT_FOLDER);
-
         const res: any[] = [];
 
-        let stream$ = folder.selectRx().pipe(
+        let stream$ = folder.selectRx('dir-1').pipe(
             rx.tap(v => res.push(v.name))
         );
         await etl.run(stream$);
@@ -66,13 +84,9 @@ describe('FTP endpoint', () => {
     });
 
     test('selectStream()', async () => {
-        const ROOT_FOLDER = 'ftp/dir-1';
-
-        const folder = ftp.getFolder(ROOT_FOLDER);
-
         const res: any[] = [];
 
-        const reader = folder.selectStream().getReader();
+        const reader = folder.selectStream('dir-1').getReader();
         let v;
         while(!(v = await reader.read()).done) res.push(v.value.name);
 
