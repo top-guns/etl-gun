75b2a2a41f57cc525a882c580c3f6b87d518a652
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-12-26 10:07:45 +0300

    Fix some exported names

--- src/lib/endpoints/index.ts
+++ src/lib/endpoints/index.ts
@@ -1,15 +1,19 @@
-export * as Errors from './errors.js'
-export * as Memory from './memory/index.js'
-export * as Interval from './interval.js'
-export * as Csv from './csv.js'
-export * as Xml from './xml.js'
-export * as Json from './json.js'
-export * as Magento from './magento/index.js'
-export * as Trello from './trello/index.js'
-export * as Zendesk from './zendesk/index.js'
-export * as messangers from './messangers/index.js'
-export * as databases from './databases/index.js'
-export * as filesystems from './filesystems/index.js'
-export * as errors from './errors.js'
-//export * as sms from './sms/index.js'
-//export * as OpenAI from './open-ai/index.js'
+export * as Magento from './magento/index.js';
+export * as Trello from './trello/index.js';
+export * as Zendesk from './zendesk/index.js';
+export * as Memory from './memory/index.js';
+
+export * as databases from './databases/index.js';
+export * as filesystems from './filesystems/index.js';
+export * as messangers from './messangers/index.js';
+export * as rest from './rest/index.js';
+
+export * as errors from './errors.js';
+
+export * as Interval from './interval.js';
+export * as Csv from './csv.js';
+export * as Xml from './xml.js';
+export * as Json from './json.js';
+
+//export * as sms from './sms/index.js';
+//export * as OpenAI from './open-ai/index.js';
--- src/lib/endpoints/rest/index.ts
+++ src/lib/endpoints/rest/index.ts
@@ -1,2 +1,4 @@
 export * from './endpoint.js';
+export * from './basic_auth_endpoint.js';
+export * from './bearer_auth_endpoin.js';
 export * from './resource_collection.js';
