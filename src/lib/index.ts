da5ed8b1e5a6c169169890e6dcb01af31e1accd5
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2023-12-28 11:29:23 +0300

    Add operators to the default export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,6 @@ export * as operators from './operators/index.js';
 export * as helpers from './helpers/index.js';
 export * as utils from './utils/index.js';
 export * as endpoints from './endpoints/index.js';
+
+// Add operators to the default export
+export * from './operators/index.js';
